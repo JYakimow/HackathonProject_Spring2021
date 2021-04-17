710e4578c0ad3bd6bb47c3202f6e0797cf3ecf02
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2021-04-17 10:59:54 -0500

    form2

--- HackathonProject_Spring2021/Form1.cs
+++ HackathonProject_Spring2021/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 using System.Windows.Forms;
 
 namespace HackathonProject_Spring2021
@@ -21,6 +22,31 @@ namespace HackathonProject_Spring2021
         {
             //test
 
+        }
+
+        private void button_qty1_Click(object sender, EventArgs e)
+        {
+            display_new_form();
+        }
+
+        public void display_new_form()
+        {
+            Form2 testDialog = new Form2();
+
+            /*
+            // Show testDialog as a modal dialog and determine if DialogResult = OK.
+            if (testDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                // Read the contents of testDialog's TextBox.
+                //this.txtResult.Text = testDialog.TextBox1.Text;
+            }
+            else
+            {
+                //this.txtResult.Text = "Cancelled";
+            }
+            */
+            testDialog.Show();
+            //testDialog.Dispose();
         }
     }
 }
