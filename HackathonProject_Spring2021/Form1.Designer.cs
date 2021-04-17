3d653d7ea2eddf36faba20ed7c112dde96287618
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2021-04-17 10:17:27 -0500

    test

--- HackathonProject_Spring2021/Form1.Designer.cs
+++ HackathonProject_Spring2021/Form1.Designer.cs
@@ -192,7 +192,8 @@ namespace HackathonProject_Spring2021
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "POS System";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
